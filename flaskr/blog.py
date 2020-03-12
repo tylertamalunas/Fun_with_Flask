bc9635ca8e1f9f61bc0dea39269b88cbac2f1c0b
tylertamalunas <tylertamalunas@gmail.com>
2020-03-12 18:30:17 -0500

    spell correction

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -55,7 +55,7 @@ def get_post(id, check_author=True):
         (id,)
     ).fetchone()
 
-    if post id None:
+    if post is None:
         abort(404, "Post id {0} doesn't exist.".format(id))
 
     if check_author and post['author_id'] != g.user['id']:
