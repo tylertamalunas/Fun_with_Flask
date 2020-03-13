b7dde512ff482248e964b00edc13c43b919baa6f
tylertamalunas <tylertamalunas@gmail.com>
2020-03-13 15:48:04 -0500

    Added a setup and MANIFEST file to make it installable.

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -46,7 +46,7 @@ def create():
 
     return render_template('blog/create.html')
 
-# Update view. 
+# Update view. Views a post and makes an Update query. 
 def get_post(id, check_author=True):
     post = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
@@ -62,3 +62,40 @@ def get_post(id, check_author=True):
         abort(403)
 
     return post
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'Post':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+            
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title = ?, body = ?'
+                ' WHERE id = ?',
+                (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/update.html', post=post)
+
+# Delete view. No template for this, instead its part of update view
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
